a9c1703cdaae9841d47952646c0f4c4fe22b5392
Jack Moon <rokmc756@gmail.com>
2023-03-18 00:51:57 +0900

    Sync

--- force_remove_hadoop_cluster.sh
+++ force_remove_hadoop_cluster.sh
@@ -14,9 +14,10 @@ ssh root@$MASTER_HOSTNAME "su hadoop -c 'stop-all.sh'"
 # for i in `echo "rk8-master rk8-slave rk8-node01 rk8-node02 rk8-node03"`
 for i in `echo "$HOSTS"`
 do
-    ssh root@$i "rm -rf /home/hadoop/* /home/hadoop/.ssh/* /tmp/*hadoop-hadoop-*.pid /usr/local/apache-zookeeper* /usr/local/kafka* /var/lib/zookeeper /var/log/zookeeper"
+    ssh root@$i "rm -rf /home/hadoop/* /tmp/*hadoop-hadoop-*.pid /usr/local/apache-zookeeper* /usr/local/kafka* /var/lib/zookeeper /var/log/zookeeper"
     ssh root@$i "systemctl stop zookeeper"
     ssh root@$i "killall java"
-    ssh root@$i "rm -rf /var/lib/zookeeper /var/log/zookeeper"
+    ssh root@$i "rm -rf /var/lib/zookeeper /var/log/zookeeper /tmp/zookeeper"
     ssh root@$i "sync; echo 3 > /proc/sys/vm/drop_caches"
 done
+#    ssh root@$i "rm -rf /home/hadoop/* /tmp/*hadoop-hadoop-*.pid /usr/local/apache-zookeeper* /usr/local/kafka* /var/lib/zookeeper /var/log/zookeeper"
