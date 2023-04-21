02b2ed15f0e8d98da457f22476b34735e590c277
Jack Moon <rokmc756@gmail.com>
2023-04-22 04:04:14 +0900

    Adding firewall role for Hadoop 3.x needed for interconnection with PXF

--- force_remove_hadoop_cluster.sh
+++ force_remove_hadoop_cluster.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-MASTER_HOSTNAME="rk8-master"
-HOSTS="rk8-master rk8-slave rk8-node01 rk8-node02 rk8-node03"
+# MASTER_HOSTNAME="rk8-master"
+# HOSTS="rk8-master rk8-slave rk8-node01 rk8-node02 rk8-node03"
 # MASTER_HOSTNAME="rk9-master"
 # $MASTER_HOSTNAME="rk9-master rk9-slave rk9-node01 rk9-node02 rk9-node03"
+MASTER_HOSTNAME="co7-master"
+ALL_HOSTS="co7-master co7-slave co7-node01 co7-node02 co7-node03"
 
-#ssh root@$MASTER_HOSTNAME su "hadoop -c 'hadoop-daemon.sh stop zkfc'"
-#ssh root@$MASTER_HOSTNAME su "hadoop -c 'stop-all.sh'"
 
-ssh root@$MASTER_HOSTNAME "su hadoop -c 'hadoop-daemon.sh stop zkfc'"
-ssh root@$MASTER_HOSTNAME "su hadoop -c 'stop-all.sh'"
+ssh root@$MASTER_HOSTNAME su "hadoop -c 'hadoop-daemon.sh stop zkfc'"
+ssh root@$MASTER_HOSTNAME su "hadoop -c 'stop-all.sh'"
+
+#ssh root@$MASTER_HOSTNAME "su hadoop -c 'hadoop-daemon.sh stop zkfc'"
+#ssh root@$MASTER_HOSTNAME "su hadoop -c 'stop-all.sh'"
 
 # for i in `echo "rk8-master rk8-slave rk8-node01 rk8-node02 rk8-node03"`
-for i in `echo "$HOSTS"`
+for i in `echo "$ALL_HOSTS"`
 do
     ssh root@$i "rm -rf /home/hadoop/* /tmp/*hadoop-hadoop-*.pid /usr/local/apache-zookeeper* /usr/local/kafka* /var/lib/zookeeper /var/log/zookeeper"
     ssh root@$i "systemctl stop zookeeper"
