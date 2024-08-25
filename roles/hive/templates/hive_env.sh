03272762d424804cba173f5193ade20afe52c21b
Jack Moon <rokmc756@gmail.com>
2024-08-26 05:39:30 +0900

    Updated for Hive Role Modified

--- roles/hive/templates/hive_env.sh
+++ roles/hive/templates/hive_env.sh
@@ -1,4 +1,4 @@
-export HIVE_HOME={{ hive_path }}/apache-hive-{{ hive_version }}-bin
+export HIVE_HOME={{ hadoop.base_path }}/{{ hive_file_name }}
 export PATH=$HIVE_HOME/bin:$PATH
 export CLASSPATH=$CLASSPATH.:$HIVE_HOME/lib
-export HIVE_CONF_DIR={{ hive_path }}/apache-hive-{{ hive_version }}-bin/conf
+export HIVE_CONF_DIR={{ hadoop.base_path }}/{{ hive_file_name }}/conf
